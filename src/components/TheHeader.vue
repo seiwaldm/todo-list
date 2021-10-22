ba2cd94fb49fc18a2d1f88c6af67938c92b1e5cd
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-10-22 08:46:07 +0200

    initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header bg-success text-white">
+    <h1>Item Lister</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  grid-column: span 3;
+  display: grid;
+  grid-template-columns: 40vw 40vw;
+  justify-content: center;
+  align-items: center;
+}
+</style>
