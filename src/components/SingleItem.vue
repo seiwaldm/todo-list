df0cccc626bd94b483d0441535aeab44564a8a0e
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-11-22 12:03:05 +0100

    added filter functionality

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="list-group-item">
+  <li v-if="isVisible" class="list-group-item">
     {{ name }}
     <button class="btn btn-danger btn-sm float-right" @click="deleteItem">
       X
@@ -10,6 +10,15 @@
 <script>
 export default {
   props: ["name"],
+  computed: {
+    isVisible() {
+      return this.$store.state.filter === ""
+        ? true
+        : this.name
+            .toLowerCase()
+            .includes(this.$store.state.filter.toLowerCase());
+    },
+  },
   methods: {
     deleteItem() {
       this.$store.dispatch("deleteItem", this.name);
